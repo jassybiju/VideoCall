4c45e3b27bf28327eed131bb9519d5d198d7c340
jassybiju <jassymon114@gmail.com>
2026-02-20 22:15:24 +0530

    Updated wss link in client

--- client/src/components/VideoRoom.tsx
+++ client/src/components/VideoRoom.tsx
@@ -12,7 +12,7 @@ const VideoRoom = () => {
   );
 
   useEffect(() => {
-    const ws = new WebSocket("wss://salty-paths-fry.loca.lt");
+    const ws = new WebSocket("wss://2a16-117-221-156-47.ngrok-free.app");
 
     ws.onopen = async () => {
       console.log("Connected to signaling server");
